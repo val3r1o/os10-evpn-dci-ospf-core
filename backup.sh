c7472f93baffb3897358f5e4c96f2edf4a12ed56
Valerio Martini <valerio.martini@gmail.com>
2021-11-22 20:49:04 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ echo ---------------------------------------------------------------------------
 echo ------------------------------------------------- Dell NETWORKING ------------------------------------
 echo ------------------------------------------------------------------------------------------------------
 
-cd dci-evpn-generic-core/
+cd os10-evpn-dci-ospf-core
 ansible-playbook -i inventory copyconfig.yml
 git status
 git pull
@@ -16,13 +16,6 @@ git push origin main
 git status
 
 echo ----------- This is an automatic backup -------------------------------------------------------------
-echo - update                                                                                          ---
-echo ---- please note on git auth changes August 2021                                                  ---
-echo - single password not accepted anymore                                                            ---
-echo - setup your token                                                                                ---
-echo - https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
-echo - remember to move from SSH to HTTPS remote git                                                   ---
-echo - https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories    ---
 echo------------------------------------------------------------------------------------------------------
 echo ---------------------------------------------- Dell NETWORKING --------------------------------------
 echo --------------------------valerio.martini@gmail.com--------------------------------------------------
